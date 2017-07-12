d7364569dc988e8a135b8a13da730674042c24ad
Lucas <lucas.hosseini@gmail.com>
2017-07-12 17:29:54 +0200

    Fix yarddoc in lib/jsonapi/renderer.rb.

--- lib/jsonapi/renderer.rb
+++ lib/jsonapi/renderer.rb
@@ -5,20 +5,20 @@ module JSONAPI
 
   # Render a JSON API document.
   #
-  # @param [Hash] params
-  #   @option [(#jsonapi_id, #jsonapi_type, #jsonapi_related, #as_jsonapi),
+  # @param params [Hash]
+  #   @option data [(#jsonapi_id, #jsonapi_type, #jsonapi_related, #as_jsonapi),
   #           Array<(#jsonapi_id, #jsonapi_type, #jsonapi_related,
   #           #as_jsonapi)>,
-  #           nil] data Primary resource(s) to be rendered.
-  #   @option [Array<#jsonapi_id>] errors Errors to be rendered.
+  #           nil] Primary resource(s) to be rendered.
+  #   @option errors [Array<#jsonapi_id>] Errors to be rendered.
   #   @option include Relationships to be included. See
   #     JSONAPI::IncludeDirective.
-  #   @option [Hash{Symbol, Array<Symbol>}, Hash{String, Array<String>}] fields
+  #   @option fields [Hash{Symbol, Array<Symbol>}, Hash{String, Array<String>}]
   #     List of requested fields for some or all of the resource types.
-  #   @option [Hash] meta Non-standard top-level meta information to be
+  #   @option meta [Hash] Non-standard top-level meta information to be
   #     included.
-  #   @option [Hash] links Top-level links to be included.
-  #   @option [Hash] jsonapi_object JSON API object.
+  #   @option links [Hash] Top-level links to be included.
+  #   @option jsonapi_object [Hash] JSON API object.
   def render(params)
     Renderer::Document.new(params).to_hash
   end
