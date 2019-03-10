3acf86b8a5a52aed670facd3ca29693b04384697
Dmytro Koval <dawidof@mail.ru>
2019-03-10 19:44:47 +0100

    Validate include directives

--- spec/include_directive_spec.rb
+++ spec/include_directive_spec.rb
@@ -2,6 +2,44 @@ require 'spec_helper'
 
 require 'jsonapi/include_directive'
 
+describe JSONAPI::IncludeDirective, '.initialize' do
+  context 'raises InvalidKey when' do
+    (
+      ["\u002B", "\u002C", "\u002E", "\u005B", "\u005D", "\u002A", "\u002F",
+       "\u0040", "\u005C", "\u005E", "\u0060"] + ("\u0021".."\u0029").to_a \
+                                               + ("\u003A".."\u003F").to_a \
+                                               + ("\u007B".."\u007F").to_a \
+                                               + ("\u0000".."\u001F").to_a \
+                                               - ['*', '.', ',']
+    ).each do |invalid_character|
+      it "invalid character provided: '#{invalid_character}'" do
+        expect { JSONAPI::IncludeDirective.new(invalid_character) }
+          .to raise_error(JSONAPI::IncludeDirective::InvalidKey)
+      end
+    end
+
+    [' ', '_', '-'].each do |char|
+      it "starts with following character: '#{char}'" do
+        expect { JSONAPI::IncludeDirective.new("#{char}_with_valid") }
+          .to raise_error(JSONAPI::IncludeDirective::InvalidKey, "#{char}_with_valid")
+      end
+
+      it "ends with following character: '#{char}'" do
+        expect { JSONAPI::IncludeDirective.new("valid_with_#{char}") }
+          .to raise_error(JSONAPI::IncludeDirective::InvalidKey, "valid_with_#{char}")
+      end
+    end
+  end
+
+  context 'not raises InvalidKey' do
+    ["\u0080", "B", "t", "5", "\u0100", "\u10FFFAA"].each do |char|
+      it "when provided characher '#{char}'" do
+        expect(JSONAPI::IncludeDirective.new(char).key?(char)).to be true
+      end
+    end
+  end
+end
+
 describe JSONAPI::IncludeDirective, '.key?' do
   it 'handles existing keys' do
     str = 'posts.comments'
